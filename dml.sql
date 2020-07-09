87026c8a389037b112ff8544223755b7b1133729
Wooooohh <42633990+Wooooohh@users.noreply.github.com>
2020-07-09 09:25:54 +0800

    Update dml.sql
    write sql code

--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
 -- Insert record
-
+insert into students values('001','Jack','22','male'),('002','Edward','21','male');
 -- Revise record
-
+update students set age = 23 where id = '002';
 --  Delete record
-
+delete from student where id = '001';
 -- Search record
+select * from student(where xxxx);
